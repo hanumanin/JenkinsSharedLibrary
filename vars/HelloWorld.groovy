f0a9a974ed5320bd37fd117879be57e26e1a05e7
hanumanin <35272625+hanumanin@users.noreply.github.com>
2020-01-11 22:53:34 -0500

    Update HelloWorld.groovy

--- vars/HelloWorld.groovy
+++ vars/HelloWorld.groovy
@@ -1,4 +1,14 @@
+import com.hanuman.package1.FirstGroovy
+
+
+
 def call(String solutionPath) {
     echo " Hello World ${solutionPath}"
+    
+    def firstGroovy = new FirstGroovy()
+    firstGroovy.testGroovyMethod()
+    
+    echo " After method Call"
+    
     
     }
