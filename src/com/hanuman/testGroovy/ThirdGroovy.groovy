af9159695f8240c0c54813ce78510a120b793bef
hanumanin <35272625+hanumanin@users.noreply.github.com>
2020-01-12 01:04:25 -0500

    Update ThirdGroovy.groovy

--- src/com/hanuman/testGroovy/ThirdGroovy.groovy
+++ src/com/hanuman/testGroovy/ThirdGroovy.groovy
@@ -1,10 +1,13 @@
 package com.hanuman.testGroovy
-
+import com.hanuman.testGroovy.SecondGroovy
 
 class ThirdGroovy implements Serializable {
   
 
   void thridMethod(){
   println " Inside ThridGroovy Class"
+    def secondGroovy = new SecondGroovy()
+   secondGroovy.testGroovyMethod1()
+    
   }
 }
