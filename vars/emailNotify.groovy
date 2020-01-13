3023e0794a80b120623db3cad1177865b0b0e1b1
hanumanin <35272625+hanumanin@users.noreply.github.com>
2020-01-12 21:18:17 -0500

    Create emailNotify.groovy

---
+++ vars/emailNotify.groovy
@@ -0,0 +1,16 @@
+def myGloablvariable;
+
+def call(String solutionPath) {
+   echo " Isndie Email Notifiy method"
+   echo " value of myGloablvariable is = ${myGloablvariable}"
+   
+    
+    }
+    
+    
+    def eamilTest(def theValue){
+    
+    echo " inside emailTest method and the vlaue is ${theValue}"
+    myGloablvariable=theValue
+    }
+    
