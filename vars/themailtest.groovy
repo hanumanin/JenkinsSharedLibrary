9b60adb5dc8b4d2a26357e3f318325cbcb19ba88
hanumanin <35272625+hanumanin@users.noreply.github.com>
2020-01-12 21:20:17 -0500

    Create themailtest.groovy

---
+++ vars/themailtest.groovy
@@ -0,0 +1,8 @@
+
+def call(String solutionPath) {
+echo " Inside the Mail test"
+      emailNotify("Calling the mail")
+    emailNotify.eamilTest("Test");  
+      
+    echo " After the mail Test"
+    }
