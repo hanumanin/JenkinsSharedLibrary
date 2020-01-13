e0fcc09a5ed6eea0966b3b5f746e4069355fa395
hanumanin <35272625+hanumanin@users.noreply.github.com>
2020-01-12 21:28:47 -0500

    Update themailtest.groovy

--- vars/themailtest.groovy
+++ vars/themailtest.groovy
@@ -1,8 +1,9 @@
 
 def call(String solutionPath) {
 echo " Inside the Mail test"
+       env.myGlobalValue="Test"
       emailNotify("Calling the mail")
-      env.myGlobalValue="Test"
+     
     emailNotify.eamilTest("Test");  
       
     echo " After the mail Test"
